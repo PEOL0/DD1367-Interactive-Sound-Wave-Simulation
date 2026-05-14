ff0c9588795d3393612ed97b091fa766f735acaf
Simon-Kahre <kahre.simon@gmail.com>
2026-05-14 09:21:02 +0200

    Make ghost object red when overlapping another object

--- sound-game/scripts/ghost_object.gd
+++ sound-game/scripts/ghost_object.gd
@@ -1,11 +1,24 @@
 extends Sprite2D
 
 var polygon: Polygon2D
+var area: Area2D
+var collision_shape: CollisionPolygon2D
+
+var can_place: bool
 
 func _ready() -> void:
 	self.modulate = Color(1,1,1,0.6)
+	polygon = Polygon2D.new()
 	self.add_child(polygon)
 	self.z_index = 2
+	
+	area = Area2D.new()
+	self.add_child(area)
+	collision_shape = CollisionPolygon2D.new()
+	area.add_child(collision_shape)
+	
+	area.connect("area_entered", area_entered)
+	area.connect("area_exited", area_exited)
 
 func _physics_process(delta: float) -> void:
 	self.global_position = get_global_mouse_position()
@@ -58,11 +71,20 @@ func create_polygon(points: PackedVector2Array) -> void:
 	polygon.polygon = points
 	self.add_child(polygon)
 
-func set_sprite(path: String, size: float = 0.053):
+func set_sprite(path: String, collision_points: PackedVector2Array, size: float = 0.053):
+	self.collision_shape.polygon = collision_points
 	self.scale = Vector2(size,size)
 	self.texture = load(path)
+	print(self.texture)
 
 func clear_polygon() -> void:
 	self.texture = null
+	self.collision_shape.polygon.clear()
 	if polygon:
 		polygon.free()
+
+func area_entered(area: Area2D):
+	self.modulate = Color(1,0.3,0.3,0.6)
+
+func area_exited(area: Area2D):
+	self.modulate = Color(1,1,1,0.6)
